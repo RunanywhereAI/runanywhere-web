787cdbacf25a51ec41eb97eb045768b23519e10b
Priyanka Khasa <priyankakhasa937@gmail.com>
2026-02-22 22:14:10 +0530

    Senior designer UI upgrade + nutrition page + posture fixes

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './styles/index.css';
+<<<<<<< HEAD
+=======
+import './features/nutrition/nutrition.css';
+import './components/app-shell.css';
+>>>>>>> d9d9034 (Senior designer UI upgrade + nutrition page + posture fixes)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
